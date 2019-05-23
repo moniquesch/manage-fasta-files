3b09c8e5f61294d64e0eebed3e35315a203bcdaa
moniquesch <49883223+moniquesch@users.noreply.github.com>
2019-05-23 18:43:37 -0300

    Add files via upload

--- getGI.m
+++ getGI.m
@@ -1,7 +1,11 @@
 function [gi] = getGI(fasta)
-% getGI  Return a list of GI numbers from a fasta file.
-%   gi = getGI(fasta)
-%   fasta = fasta file.
+% getGI  Returns a list of the GI numbers found on the fasta file.
+%   gi = getGI(fasta) is a string array containing the name of the species on the FASTA file.
+
+%   It is important that the fasta file is in the NCBI format. In other
+%   words, the gi number must to be present and between pipes. For example:
+%   gi|12345|
+
 gi = arrayfun(@(x) string(extractBetween(x.Header, 'gi|', '|')),fasta);
 end
 
